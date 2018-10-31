8fb6beb72d159be7b1a86b62701dc8274be9619e
Ruben Taelman <ruben.taelman@ugent.be>
2018-10-31 15:28:50 +0100

    Depend on relative-to-absolute-iri for IRI resolving

--- lib/RdfXmlParser.ts
+++ lib/RdfXmlParser.ts
@@ -1,4 +1,5 @@
 import * as RDF from "rdf-js";
+import {resolve} from "relative-to-absolute-iri";
 import {createStream, SAXStream, Tag} from "sax";
 import {Transform, TransformCallback} from "stream";
 
@@ -157,100 +158,7 @@ export class RdfXmlParser extends Transform {
    * @return {NamedNode} an IRI.
    */
   public valueToUri(value: string, activeTag: IActiveTag): RDF.NamedNode {
-    let baseIRI: string = activeTag.baseIRI || '';
-    const baseFragmentPos: number = baseIRI.indexOf('#');
-
-    // Ignore any fragments in the base IRI
-    if (baseFragmentPos > 0) {
-      baseIRI = baseIRI.substr(0, baseFragmentPos);
-    }
-
-    // Convert empty value directly to base IRI
-    if (!value.length) {
-      return this.dataFactory.namedNode(baseIRI);
-    }
-
-    // If the value starts with a hash, concat directly
-    if (value.startsWith('#')) {
-      return this.dataFactory.namedNode(baseIRI + value);
-    }
-
-    // Ignore baseIRI if it is empty
-    if (!baseIRI.length) {
-      return this.dataFactory.namedNode(value);
-    }
-
-    // Ignore baseIRI if the value is absolute
-    const valueColonPos: number = value.indexOf(':');
-    if (valueColonPos >= 0) {
-      return this.dataFactory.namedNode(value);
-    }
-
-    // At this point, the baseIRI MUST be absolute, otherwise we error
-    const baseColonPos: number = baseIRI.indexOf(':');
-    if (baseColonPos < 0) {
-      throw new Error(`Found invalid baseIRI '${baseIRI}' for value '${value}'`);
-    }
-
-    const baseIRIScheme = baseIRI.substr(0, baseColonPos + 1);
-    // Inherit the baseIRI scheme if the value starts with '//'
-    if (value.indexOf('//') === 0) {
-      return this.dataFactory.namedNode(baseIRIScheme + value);
-    }
-
-    // Check cases where '://' occurs in the baseIRI, and where there is no '/' after a ':' anymore.
-    let baseSlashAfterColonPos;
-    if (baseIRI.indexOf('//', baseColonPos) === baseColonPos + 1) {
-      // If there is no additional '/' after the '//'.
-      baseSlashAfterColonPos = baseIRI.indexOf('/', baseColonPos + 3);
-      if (baseSlashAfterColonPos < 0) {
-        // If something other than a '/' follows the '://', append the value after a '/',
-        // otherwise, prefix the value with only the baseIRI scheme.
-        if (baseIRI.length > baseColonPos + 3) {
-          return this.dataFactory.namedNode(baseIRI + '/' + value);
-        } else {
-          return this.dataFactory.namedNode(baseIRIScheme + value);
-        }
-      }
-    } else {
-      // If there is not even a single '/' after the ':'
-      baseSlashAfterColonPos = baseIRI.indexOf('/', baseColonPos + 1);
-      // Always true: baseSlashAfterColonPos < 0
-      // If something other than a '/' follows the ':', append the value after a '/',
-      // otherwise, prefix the value with only the baseIRI scheme.
-      if (baseIRI.length > baseColonPos + 1) {
-        return this.dataFactory.namedNode(baseIRI + '/' + value);
-      } else {
-        return this.dataFactory.namedNode(baseIRIScheme + value);
-      }
-    }
-
-    // If the value starts with a '/', then prefix it with everything before the first effective slash of the base IRI.
-    if (value.indexOf('/') === 0) {
-      return this.dataFactory.namedNode(baseIRI.substr(0, baseSlashAfterColonPos) + value);
-    }
-
-    let baseIRIPath = baseIRI.substr(baseSlashAfterColonPos);
-    const baseIRILastSlashPos = baseIRIPath.lastIndexOf('/');
-
-    // Ignore everything after the last '/' in the baseIRI path
-    if (baseIRILastSlashPos >= 0 && baseIRILastSlashPos < baseIRIPath.length - 1) {
-      baseIRIPath = baseIRIPath.substr(0, baseIRILastSlashPos + 1);
-    }
-
-    // Prefix the value with the baseIRI path where
-    value = baseIRIPath + value;
-    // Remove all occurrences of '*/../' to collapse paths to parents
-    while (value.match(/[^\/]*\/\.\.\//)) {
-      value = value.replace(/[^\/]*\/\.\.\//, '');
-    }
-    // Remove all occurrences of './'
-    value = value.replace(/\.\//g, '');
-    // Remove suffix '/.'
-    value = value.replace(/\/\.$/, '/');
-
-    // Prefix our transformed value with the part of the baseIRI until the first '/' after the first ':'.
-    return this.dataFactory.namedNode(baseIRI.substr(0, baseSlashAfterColonPos) + value);
+    return this.dataFactory.namedNode(resolve(value, activeTag.baseIRI));
   }
 
   /**
