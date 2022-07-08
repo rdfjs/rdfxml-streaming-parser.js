a0cba55c65a8e829554e97edfd4b5736dcf4f72e
Thomas Tanon <thomaspt@hotmail.fr>
2022-07-08 14:07:58 +0200

    Fix incorrect arrayify-stream call

--- spec/parser.js
+++ spec/parser.js
@@ -2,6 +2,6 @@ const RdfXmlParser = require("..").RdfXmlParser;
 
 module.exports = {
   parse: function (data, baseIRI) {
-    return require('arrayify-stream')(require('streamify-string')(data).pipe(new RdfXmlParser({ baseIRI })));
+    return require('arrayify-stream').default(require('streamify-string')(data).pipe(new RdfXmlParser({ baseIRI })));
   },
 };
